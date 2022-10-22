da24b9b11cc49a4c95232dca8a91ee5eeba32e76
Rodrigo <rodriept429@gmail.com>
2022-10-22 03:19:05 -0600

    Ingreso de modo de dificultad del juego y movimientos permitidos por dificultad

--- Color_Mania/Color_Mania/MyForm.h
+++ Color_Mania/Color_Mania/MyForm.h
@@ -52,6 +52,11 @@ namespace ColorMania {
 	private: System::Windows::Forms::Button^ BIniciarJuego;
 	private: System::Windows::Forms::DataGridView^ dataGridView1;
 	private: System::Windows::Forms::Button^ BBorrarMapa;
+	private: System::Windows::Forms::ComboBox^ CBElegirModo;
+
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ LMovimientosRes;
 
 
 
@@ -69,6 +74,7 @@ namespace ColorMania {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->BIngresarArchivo = (gcnew System::Windows::Forms::Button());
@@ -77,12 +83,17 @@ namespace ColorMania {
 			this->BIniciarJuego = (gcnew System::Windows::Forms::Button());
 			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
 			this->BBorrarMapa = (gcnew System::Windows::Forms::Button());
+			this->CBElegirModo = (gcnew System::Windows::Forms::ComboBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->LMovimientosRes = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
+			this->label1->BackColor = System::Drawing::SystemColors::Info;
 			this->label1->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
 			this->label1->FlatStyle = System::Windows::Forms::FlatStyle::System;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Franklin Gothic Medium Cond", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
@@ -97,6 +108,8 @@ namespace ColorMania {
 			// label3
 			// 
 			this->label3->AutoSize = true;
+			this->label3->BackColor = System::Drawing::SystemColors::HighlightText;
+			this->label3->ForeColor = System::Drawing::SystemColors::Desktop;
 			this->label3->Location = System::Drawing::Point(11, 59);
 			this->label3->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label3->Name = L"label3";
@@ -106,14 +119,16 @@ namespace ColorMania {
 			// 
 			// BIngresarArchivo
 			// 
+			this->BIngresarArchivo->BackColor = System::Drawing::SystemColors::MenuBar;
 			this->BIngresarArchivo->FlatStyle = System::Windows::Forms::FlatStyle::Popup;
+			this->BIngresarArchivo->ForeColor = System::Drawing::SystemColors::Desktop;
 			this->BIngresarArchivo->Location = System::Drawing::Point(225, 54);
 			this->BIngresarArchivo->Margin = System::Windows::Forms::Padding(2);
 			this->BIngresarArchivo->Name = L"BIngresarArchivo";
 			this->BIngresarArchivo->Size = System::Drawing::Size(114, 22);
 			this->BIngresarArchivo->TabIndex = 5;
 			this->BIngresarArchivo->Text = L"Ingresar Archivo";
-			this->BIngresarArchivo->UseVisualStyleBackColor = true;
+			this->BIngresarArchivo->UseVisualStyleBackColor = false;
 			this->BIngresarArchivo->Click += gcnew System::EventHandler(this, &MyForm::BIngresarArchivo_Click);
 			// 
 			// OFDAbrirArchivo
@@ -122,17 +137,19 @@ namespace ColorMania {
 			// 
 			// BSalir
 			// 
+			this->BSalir->BackColor = System::Drawing::SystemColors::MenuBar;
 			this->BSalir->FlatStyle = System::Windows::Forms::FlatStyle::Popup;
 			this->BSalir->Location = System::Drawing::Point(406, 365);
 			this->BSalir->Name = L"BSalir";
 			this->BSalir->Size = System::Drawing::Size(67, 24);
 			this->BSalir->TabIndex = 6;
 			this->BSalir->Text = L"Salir ";
-			this->BSalir->UseVisualStyleBackColor = true;
+			this->BSalir->UseVisualStyleBackColor = false;
 			this->BSalir->Click += gcnew System::EventHandler(this, &MyForm::BSalir_Click);
 			// 
 			// BIniciarJuego
 			// 
+			this->BIniciarJuego->BackColor = System::Drawing::SystemColors::MenuBar;
 			this->BIniciarJuego->Enabled = false;
 			this->BIniciarJuego->FlatStyle = System::Windows::Forms::FlatStyle::Popup;
 			this->BIniciarJuego->Location = System::Drawing::Point(237, 81);
@@ -140,7 +157,7 @@ namespace ColorMania {
 			this->BIniciarJuego->Size = System::Drawing::Size(87, 25);
 			this->BIniciarJuego->TabIndex = 7;
 			this->BIniciarJuego->Text = L"Inciar Juego";
-			this->BIniciarJuego->UseVisualStyleBackColor = true;
+			this->BIniciarJuego->UseVisualStyleBackColor = false;
 			this->BIniciarJuego->Click += gcnew System::EventHandler(this, &MyForm::BIniciarJuego_Click);
 			// 
 			// dataGridView1
@@ -160,20 +177,68 @@ namespace ColorMania {
 			// 
 			// BBorrarMapa
 			// 
+			this->BBorrarMapa->BackColor = System::Drawing::SystemColors::MenuBar;
 			this->BBorrarMapa->FlatStyle = System::Windows::Forms::FlatStyle::Popup;
 			this->BBorrarMapa->Location = System::Drawing::Point(299, 365);
 			this->BBorrarMapa->Name = L"BBorrarMapa";
 			this->BBorrarMapa->Size = System::Drawing::Size(101, 24);
 			this->BBorrarMapa->TabIndex = 9;
 			this->BBorrarMapa->Text = L"Borrar Mapa";
-			this->BBorrarMapa->UseVisualStyleBackColor = true;
+			this->BBorrarMapa->UseVisualStyleBackColor = false;
 			this->BBorrarMapa->Click += gcnew System::EventHandler(this, &MyForm::BBorrarMapa_Click);
+			// 
+			// CBElegirModo
+			// 
+			this->CBElegirModo->FormattingEnabled = true;
+			this->CBElegirModo->Items->AddRange(gcnew cli::array< System::Object^  >(3) { L"Fácil", L"Medio", L"Difícil" });
+			this->CBElegirModo->Location = System::Drawing::Point(356, 85);
+			this->CBElegirModo->Name = L"CBElegirModo";
+			this->CBElegirModo->Size = System::Drawing::Size(112, 21);
+			this->CBElegirModo->TabIndex = 10;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->BackColor = System::Drawing::SystemColors::Window;
+			this->label2->ForeColor = System::Drawing::SystemColors::Desktop;
+			this->label2->Location = System::Drawing::Point(353, 69);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(110, 13);
+			this->label2->TabIndex = 11;
+			this->label2->Text = L"Elegir Modo de juego:";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->BackColor = System::Drawing::SystemColors::Window;
+			this->label4->ForeColor = System::Drawing::SystemColors::Desktop;
+			this->label4->Location = System::Drawing::Point(363, 138);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(69, 13);
+			this->label4->TabIndex = 12;
+			this->label4->Text = L"Movimientos:";
+			// 
+			// LMovimientosRes
+			// 
+			this->LMovimientosRes->AutoSize = true;
+			this->LMovimientosRes->BackColor = System::Drawing::SystemColors::Window;
+			this->LMovimientosRes->ForeColor = System::Drawing::SystemColors::Desktop;
+			this->LMovimientosRes->Location = System::Drawing::Point(432, 138);
+			this->LMovimientosRes->Name = L"LMovimientosRes";
+			this->LMovimientosRes->Size = System::Drawing::Size(0, 13);
+			this->LMovimientosRes->TabIndex = 13;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(485, 401);
+			this->BackColor = System::Drawing::SystemColors::GradientInactiveCaption;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->ClientSize = System::Drawing::Size(499, 401);
+			this->Controls->Add(this->LMovimientosRes);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->CBElegirModo);
 			this->Controls->Add(this->BBorrarMapa);
 			this->Controls->Add(this->dataGridView1);
 			this->Controls->Add(this->BIniciarJuego);
@@ -194,6 +259,7 @@ namespace ColorMania {
 		int ContPilas = 0;
 		int Clicks = 0, FilaDGV = 0, ColumnaDGV = 0;
 		int pilaMax1 = 0, pilaMax2 = 0, pilaMax3 = 0, pilaMax4 = 0, pilaMax;
+		int Movimientos, opcionModo;
 	private: System::Void BIngresarArchivo_Click(System::Object^ sender, System::EventArgs^ e) 
 	{
 		array<String^>^ DatosTemporales;
@@ -492,6 +558,25 @@ namespace ColorMania {
             }
 			ContPilas--;
 		}
+		opcionModo = CBElegirModo->SelectedIndex;
+		switch (opcionModo)
+		{
+		case 0:
+			Movimientos = 100000;
+			LMovimientosRes->Text = "infinito";
+			break;
+		case 1:
+			Movimientos = 50;
+			LMovimientosRes->Text = Convert::ToString(Movimientos);
+			break;
+		case 2:
+			Movimientos = 25;
+			LMovimientosRes->Text = Convert::ToString(Movimientos);
+			break;
+		default:
+			break;
+		}
+
 		pilaMax = 0;
 		pilaMax1 = 0;
 		pilaMax2 = 0;
@@ -502,50 +587,51 @@ namespace ColorMania {
     }
     private: System::Void dataGridView1_CellClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e)
     {
-		
-		if (Clicks == 0)
+		if (Movimientos == 0)
 		{
-			if (dataGridView1->Rows[e->RowIndex - 1]->Cells[e->ColumnIndex]->Style->BackColor != Color::White)
-			{
-				MessageBox::Show("Casilla no permitida porque tiene una arriba de el", "Error: Casilla no permitida", MessageBoxButtons::OK, MessageBoxIcon::Error);
-
-			}
-			else
-			{
-				MessageBox::Show("Presione donde quiere ingresar el color", "Ingrese la casilla", MessageBoxButtons::OK);
-				FilaDGV = e->RowIndex;
-				ColumnaDGV = e->ColumnIndex;
-				
-				Clicks++;
-			}
+			MessageBox::Show("Ya no tienes mas movimientos terminó el juego", "Juego terminado", MessageBoxButtons::OK);
 		}
-		else if (Clicks == 1)
+		else
 		{
-
-			if (dataGridView1->Rows[e->RowIndex]->Cells[e->ColumnIndex]->Style->BackColor != Color::White)
+			if (Clicks == 0)
 			{
-				MessageBox::Show("Lugar ya ocupado por un color, Ingrese el color en otra casilla", "Error: Ingrese el color en otra casilla", MessageBoxButtons::OK, MessageBoxIcon::Error);
-			}
-			else
-			{
-				if (dataGridView1->Rows[e->RowIndex + 1]->Cells[e->ColumnIndex]->Style->BackColor != Color::White)
+				if (dataGridView1->Rows[e->RowIndex - 1]->Cells[e->ColumnIndex]->Style->BackColor != Color::White)
 				{
-					MessageBox::Show("No se puede ingresar en esa casilla, porque no posee un color abajo", "Error: Lugar incorrecto", MessageBoxButtons::OK, MessageBoxIcon::Error);
-					Clicks--;
+					MessageBox::Show("Casilla no permitida porque tiene una arriba de el", "Error: Casilla no permitida", MessageBoxButtons::OK, MessageBoxIcon::Error);
+
 				}
 				else
 				{
+					MessageBox::Show("Presione donde quiere ingresar el color", "Ingrese la casilla", MessageBoxButtons::OK);
+					FilaDGV = e->RowIndex;
+					ColumnaDGV = e->ColumnIndex;
+					Clicks++;
+				}
+			}
+			else if (Clicks == 1)
+			{
+
+				if (dataGridView1->Rows[e->RowIndex]->Cells[e->ColumnIndex]->Style->BackColor != Color::White)
+				{
+					MessageBox::Show("Lugar ya ocupado por un color, Ingrese el color en otra casilla", "Error: Ingrese el color en otra casilla", MessageBoxButtons::OK, MessageBoxIcon::Error);
+				}
+				else
+				{
+
 					dataGridView1->Rows[e->RowIndex]->Cells[e->ColumnIndex]->Style->BackColor = dataGridView1->Rows[FilaDGV]->Cells[ColumnaDGV]->Style->BackColor;
 					dataGridView1->Rows[FilaDGV]->Cells[ColumnaDGV]->Style->BackColor = Color::White;
+
+					if (opcionModo != 0)
+					{
+						Movimientos--;
+						LMovimientosRes->Text = Convert::ToString(Movimientos);
+					}
 					Clicks--;
+
+
 				}
-				
 			}
 		}
-		
-		
-			
-			
 			
 		
     }
